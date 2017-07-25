fefc2acb846adc153f05a36e67322990620181ab
Aaron Frey <aaron.frey@gmail.com>
2017-07-25 18:00:23 -0400

    add shared resources

--- admin/class-pepperlillie-nearby-locations-admin.php
+++ admin/class-pepperlillie-nearby-locations-admin.php
@@ -60,10 +60,8 @@ class Pepperlillie_Nearby_Locations_Admin {
 	 * @since    1.0.0
 	 */
 	public function enqueue_styles() {
-
-		wp_enqueue_style( 'jquery-ui', 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css', array(), $this->version, 'all' );
-
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/pepperlillie-nearby-locations-admin.css', array(), $this->version, 'all' );
+		wp_enqueue_style('jquery-ui', 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css', array(), $this->version, 'all');
+		wp_enqueue_style($this->plugin_name, plugin_dir_url(__FILE__) . 'css/pepperlillie-nearby-locations-admin.css', array(), $this->version, 'all');
 	}
 
 	/**
@@ -72,15 +70,13 @@ class Pepperlillie_Nearby_Locations_Admin {
 	 * @since    1.0.0
 	 */
 	public function enqueue_scripts() {
-		
 		wp_enqueue_script('google-maps', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDMkqWm3kT5wOXT-400rN7sURd-sFIR2hI', array(), $this->version, false);
-
 		wp_enqueue_script('jquery-ui', 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js', array('jquery'), $this->version, false);
 
-		wp_enqueue_script($this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/pepperlillie-nearby-locations-admin.js', array( 'jquery' ), $this->version, false );
+		wp_enqueue_script('shared', plugin_dir_url(dirname(__FILE__)) . 'shared/js/pepperlillie-nearby-locations-shared.js', array('jquery'), $this->version, false);
+		wp_localize_script('shared', 'myVars', array('ajaxUrl' => admin_url('admin-ajax.php')));
 
-		wp_localize_script($this->plugin_name, 'myVars', array('ajaxUrl' => admin_url('admin-ajax.php')));
-		
+		wp_enqueue_script($this->plugin_name, plugin_dir_url(__FILE__) . 'js/pepperlillie-nearby-locations-admin.js', array('jquery'), $this->version, false);
 	}
 
 	public function pepperlillie_nearby_locations_page() {
--- admin/js/pepperlillie-nearby-locations-admin.js
+++ admin/js/pepperlillie-nearby-locations-admin.js
@@ -2,145 +2,7 @@
 
     'use strict';
 
-    var activeMarker = null,
-        bounds,
-        geocoder,
-        infowindow,
-        locations,
-        map,
-        markers = [],
-        toggleAll = true;
-
-    // return an array key based on value
-    var arraySearch = function(array, value) {
-        for (var i = 0; i < array.length; i++) {
-            if (array[i] === value) return i;
-        }
-        return false;
-    }
-
-    // get locations from the database
-    // loop through and populate the map with location markers
-    var fetchPlaces = function() {
-
-        jQuery.ajax({
-            url: myVars.ajaxUrl,
-            dataType: 'json',
-            type: 'post',
-            data: {
-                'action': 'nearby_locations_crud',
-                'callback': 'get_locations'
-            },
-            cache: false,
-            success: function(response) {
-
-                locations = response;
-
-                // create a bounds to contain all markers on the map
-                bounds = new google.maps.LatLngBounds();
-
-                // loop through locations and add markers to map
-                for (var l in locations) {
-                    // make and place map maker
-                    var marker = new google.maps.Marker({
-                        map: map,
-                        position: new google.maps.LatLng(locations[l].lat, locations[l].lng),
-                        title: locations[l].name + "<br>" + locations[l].geo_name
-                    });
-                    markers.push(marker);
-
-                    // add marker to the contained bounds
-                    bounds.extend(marker.getPosition());
-
-                    // bind click event to show the info box
-                    bindInfoWindow(marker, map, infowindow, '<b>' + locations[l].name + "</b><br>" + locations[l].formatted);
-                }
-                // readjust the map to fit all the markers at once
-                map.fitBounds(bounds);
-            }
-        })
-    };
-
-    // binds a map marker and infoWindow together on click
-    var bindInfoWindow = function(marker, map, infowindow, html) {
-        google.maps.event.addListener(marker, 'click', function() {
-            // show the info window
-            infowindow.setContent(html);
-            infowindow.open(map, marker);
-            // animate the selected marker
-            animateMarker(arraySearch(markers, marker));
-        });
-    }
-
-    var animateMarker = function(index) {
-        if (index !== activeMarker) {
-            if (activeMarker !== null) {
-                markers[activeMarker].setAnimation(null);
-            }
-            markers[index].setAnimation(google.maps.Animation.BOUNCE);
-            activeMarker = index;
-        }
-    }
-
-    function initialize() {
-        // create the geocoder
-        geocoder = new google.maps.Geocoder();
-
-        // create the infowindow
-        infowindow = new google.maps.InfoWindow({
-            content: ''
-        });
-
-        // add listener to stop marker animation on infobox close
-        google.maps.event.addListener(infowindow, 'closeclick', function() {
-            markers[activeMarker].setAnimation(null);
-        });
-
-        if ($('body').hasClass('plugins-nearby-locations-admin-partials-pepperlillie-nearby-locations-admin-display-php')) {
-
-            // set some default map details, initial center point, zoom and style
-            var mapOptions = {
-                center: new google.maps.LatLng(39.9523789, -75.1657883),
-                zoom: 12,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            };
-
-            // create the map and reference the div#map-canvas container
-            map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-
-            // fetch the existing places (ajax) and put them on the map
-            fetchPlaces();
-        }
-    }
-
-    // when page is ready, initialize the map!
-    google.maps.event.addDomListener(window, 'load', initialize);
-
     $(function() {
-
-        // initialize the location types accordion 
-        $('.accordion').accordion({
-            active: false,
-            collapsible: true,
-            heightStyle: 'content',
-            icons: {
-                "header": "ui-icon-triangle-1-s",
-                "activeHeader": "ui-icon-triangle-1-n"
-            },
-            beforeActivate: function(event, ui) {
-                if (!toggleAll && !ui.newHeader.size()) {
-                    return false;
-                }
-
-                toggleAll = false;
-            }
-        });
-
-        $('#toggle-all').on('click', function(e) {
-            e.preventDefault();
-            toggleAll = true;
-            $('.ui-accordion-header-active').click();
-        });
 
         $('form#location-type-form').submit(function(e) {
 
@@ -161,12 +23,10 @@
                 data: data,
                 cache: false,
                 success: function(response) {
-                    console.log(response);
-                    // reload the page
+                    // reload the page - TEMPORARY
                     location.reload();
                 },
                 error: function(response) {
-                    console.log(response);
                     $('#message').html('Try again. Saving location was not successful.');
                 }
             });
@@ -209,13 +69,11 @@
                         data: data,
                         cache: false,
                         success: function(response) {
-                            console.log(response);
                             // reload the page
                             location.reload();
                         },
                         error: function(response) {
                             $('#message').html('Try again. Saving location was not successful.');
-                            console.log('error');
                         }
                     });
 
@@ -224,14 +82,6 @@
                 }
             });
         });
-
-        $('.location-link').on('click', function(e) {
-            e.preventDefault();
-            var index = $(this).data('location-index');
-            google.maps.event.trigger(markers[index], 'click');
-            animateMarker(index);
-        });
-
     });
 
 })(jQuery);
