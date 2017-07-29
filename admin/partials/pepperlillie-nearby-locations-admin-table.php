495b65c6cbff50d957961486cd3aa9927143d083
Aaron Frey <aaron.frey@gmail.com>
2017-07-29 17:49:52 -0400

    add delete capabilitites

--- admin/partials/pepperlillie-nearby-locations-admin-table.php
+++ admin/partials/pepperlillie-nearby-locations-admin-table.php
@@ -1,8 +1,10 @@
 <?php
 
-if(!class_exists('WP_List_Table')) {
+if (!class_exists('WP_List_Table')) {
   require_once(ABSPATH . 'wp-admin/includes/class-wp-list-table.php');
 }
+
+// var_dump($_GET);
 
 class TT_Example_List_Table extends WP_List_Table {
     
@@ -11,33 +13,35 @@ class TT_Example_List_Table extends WP_List_Table {
     global $status, $page;
 
     parent::__construct(array(
-      'singular'  => 'nearby location',
-      'plural'    => 'nearby locations',
+      'singular'  => 'nearby_location',
+      'plural'    => 'nearby_locations',
       'ajax'      => false
     ));
   }
 
   function column_default($item, $column_name) {
     switch($column_name) {
-      case 'column_name':
+      case 'section_name':
+      case 'formatted':
+      case 'lat':
+      case 'lng':
         return $item[$column_name];
       default:
         return print_r($item,true);
     }
   }
 
-  function column_name($item){      
+  function column_name($item) {      
     // Build row actions
     $actions = array(
-      'edit'   => sprintf('<a href="?page=%s&action=%s&location=%s">Edit</a>',$_REQUEST['page'],'edit',$item['id']),
-      'delete' => sprintf('<a href="?page=%s&action=%s&location=%s">Delete</a>',$_REQUEST['page'],'delete',$item['id']),
+      //'edit'   => sprintf('<a href="?page=%s&action=%s&location=%s">Edit</a>',$_REQUEST['page'],'edit',$item['id']),
+      'delete' => sprintf('<a href="?page=%s&action=%s&'.$this->_args['singular'].'=%s">Delete</a>',$_REQUEST['page'],'delete',$item['id']),
     );
     
     // Return the title contents
-    return sprintf('%1$s <span style="color:silver">(id:%2$s)</span>%3$s',
+    return sprintf('%1$s%2$s',
       /*$1%s*/ $item['name'],
-      /*$2%s*/ $item['id'],
-      /*$3%s*/ $this->row_actions($actions)
+      /*$2%s*/ $this->row_actions($actions)
     );
   }
 
@@ -51,15 +55,20 @@ class TT_Example_List_Table extends WP_List_Table {
 
   function get_columns(){
     $columns = array(
-      'cb'    => '<input type="checkbox" />',
-      'name'  => 'Location Name',
+      'cb'            => '<input type="checkbox" />',
+      'name'          => 'Location Name',
+      'section_name'  => 'Location Type',
+      'formatted'     => 'Location Address',
+      'lng'           => 'Longitude',
+      'lat'           => 'Latitude',
     );
     return $columns;
   }
 
   function get_sortable_columns() {
     $sortable_columns = array(
-      'name' => array('name', false)
+      'name'          => array('name', false),
+      'section_name'  => array('section_name', false)
     );
     return $sortable_columns;
   }
@@ -71,11 +80,21 @@ class TT_Example_List_Table extends WP_List_Table {
     return $actions;
   }
 
-  function process_bulk_action() {      
-    // Detect when a bulk action is being triggered...
+  function process_bulk_action() {
+    
+    global $wpdb;
+
+    // get the location(s) to perform actions on
+    $location = $_GET[$this->_args['singular']];
+    if (!is_array($location)) {
+      $location = [$location];
+    }
+
+    $table_name = $wpdb->prefix . "plnl_locations"; 
+
+    // detect when a bulk action is being triggered...
     if ('delete' === $this->current_action()) {
-    	var_dump($_POST);
-      //wp_die('Items deleted (or they would be if we had items to delete)!');
+      $wpdb->query("DELETE FROM $table_name WHERE id IN (" . implode(",", $location) . ")");
     }
   }
 
@@ -90,8 +109,14 @@ class TT_Example_List_Table extends WP_List_Table {
     $this->_column_headers = array($columns, $hidden, $sortable);
     $this->process_bulk_action();
 
-    $table_name = $wpdb->prefix . "plnl_locations"; 
-    $data = $wpdb->get_results("SELECT * FROM  $table_name", "ARRAY_A");
+    $table_name = $wpdb->prefix . "plnl_sections"; 
+    $join_table_name = $wpdb->prefix . "plnl_locations"; 
+    $data = $wpdb->get_results("
+      SELECT `locations`.*, `sections`.name `section_name`
+      FROM $table_name `sections`, $join_table_name `locations`
+      WHERE `locations`.`section_id` = `sections`.`id`
+      ORDER BY `sections`.`order` ASC, `locations`.name
+    ", "ARRAY_A");
 
     function usort_reorder($a,$b) {
       $orderby = (!empty($_REQUEST['orderby'])) ? $_REQUEST['orderby'] : 'name';
