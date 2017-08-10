1605a4328b04651d32651f0dd1d67a3c88b4ac10
Aaron Frey <aaron.frey@gmail.com>
2017-08-09 23:28:51 -0400

    fix adding uncategorized locations to map

--- shared/partials/pepperlillie-nearby-locations-map-display.php
+++ shared/partials/pepperlillie-nearby-locations-map-display.php
@@ -14,8 +14,8 @@ $table_name = $wpdb->prefix . "plnl_sections";
 $join_table_name = $wpdb->prefix . "plnl_locations"; 
 $locations = $wpdb->get_results("
   SELECT `locations`.*, `sections`.name `section_name`
-  FROM $table_name `sections`, $join_table_name `locations`
-  WHERE `locations`.`section_id` = `sections`.`id`
+  FROM $join_table_name `locations`
+  LEFT JOIN $table_name `sections` ON `sections`.id = `locations`.section_id
   ORDER BY `sections`.`order` ASC, `locations`.name
 ", OBJECT);
 
@@ -36,38 +36,49 @@ $locations = $wpdb->get_results("
         <?php
 
         $current_location_type = '';
+        $first = true;
 
         foreach ($locations as $idx => $location) :
 
-          if ($location->section_id !== $current_location_type) : $current_location_type = $location->section_id; ?>
+          if ($location->section_id != "-99") :
 
-            <!-- if this is not the first location, close all previous opened tags -->
-            <?php if ($idx !== 0) : ?>
-              </ul></div>
-            <?php endif; ?>
+            if ($location->section_id !== $current_location_type) :
 
-            <h3 data-section-id="<?php echo esc_attr($location->section_id); ?>">
-              <?php echo esc_html($location->section_name); ?>
-            </h3>
-            <div>
-              <ul>
-                <li>
-                  <a href="#" class="location-link" data-location-index="<?php echo esc_attr($idx); ?>">
-                    <?php echo esc_html($location->name); ?>
-                  </a>
-                </li>
+              $current_location_type = $location->section_id; ?>
 
-              <?php else : ?>
-                <li>
-                  <a href="#" class="location-link" data-location-index="<?php echo esc_attr($idx); ?>">
-                    <?php echo esc_html($location->name); ?>
-                  </a>
-                </li>
+              <!-- if this is not the first location, close all previous opened tags -->
+              <?php if ($first !== true) : $first = false; ?>
+                </ul></div>
               <?php endif; ?>
+
+              <h3 data-section-id="<?php echo esc_attr($location->section_id); ?>">
+                <?php echo esc_html($location->section_name); ?>
+              </h3>
+              <div>
+                <ul>
+                  <li>
+                    <a href="#" class="location-link" data-location-index="<?php echo esc_attr($idx); ?>">
+                      <?php echo esc_html($location->name); ?>
+                    </a>
+                  </li>
+
+                <?php else : ?>
+                  <li>
+                    <a href="#" class="location-link" data-location-index="<?php echo esc_attr($idx); ?>">
+                      <?php echo esc_html($location->name); ?>
+                    </a>
+                  </li>
+                <?php endif; ?>
+
+            <?php endif ;?>
 
         <?php endforeach; ?>
 
-        </ul></div>
+        <?php if ($location->section_id != "-99") : ?>
+
+          </ul></div>
+
+        <?php endif; ?> 
 
       </div><!-- .accordion -->
 
